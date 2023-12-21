c89b329941059d6ef8aba143a31c6c3aa7a7a0aa
Yaseen Reza <57412770+yaseen157@users.noreply.github.com>
2023-12-21 22:19:07 +0000

    unsafe commit, Mattingly turbo decks added

--- ADRpy/propulsion.py
+++ ADRpy/propulsion.py
@@ -5,6 +5,7 @@ decks" - derived from real engine data.
 import os
 import re
 import typing
+import warnings
 
 import numpy as np
 import pandas as pd
@@ -12,7 +13,10 @@ from scipy.interpolate import interp1d, CloughTocher2DInterpolator
 
 from ADRpy import atmospheres as at
 from ADRpy.mtools4acdc import recastasnpfloatarray
+from ADRpy import unitconversions as uc
 
+__all__ = ["engine_catalogue", "EngineDeck", "TurbofanHiBPR", "TurbofanLoBPR",
+           "Turbojet", "Turboprop"]
 __author__ = "Yaseen Reza"
 
 # Other contributors: Samuel Pearson
@@ -54,22 +58,58 @@ class EngineDeck:
     _f_thrust = NotImplemented
     _f_thrust_SLTO = NotImplemented
 
-    def __init__(self, enginename: str):
+    def __new__(cls, engine: str):
 
-        if enginename not in engine_catalogue:
+        validtypes = {  # generic deck types
+            "turbofanHiBPR": TurbofanHiBPR,
+            "turbofanLoBPR": TurbofanLoBPR,
+            "turbojet": Turbojet,
+            "turboprop": Turboprop,
+            "piston": Piston
+        }
+        # If the engine specified is actually in the form "type:<x>"
+        if engine.startswith("type:"):
+
+            enginetype = engine.lstrip("type:")
+            genericclass = validtypes.get(enginetype)
+
+            if genericclass is None:
+                errormsg = f"{engine=} not recognised. Try one of {validtypes=}"
+                raise ValueError(errormsg)
+            return genericclass()
+
+        # Default behaviour, create an instance and then run the __init__ method
+        instance = super(EngineDeck, cls).__new__(cls)
+        instance.__init__(engine)
+        return instance
+
+    def __init__(self, engine: str):
+        """
+        Args:
+            engine: Name of an engine for which data exists, or a string in the
+                form "type:<x>" where <x> is any of the following:
+
+                - turbofanHiBPR
+                - turbofanLoBPR
+                - turbojet
+                - turboprop
+                - piston
+
+        """
+        if engine not in engine_catalogue:
             errormsg = (
-                f"'{enginename}' not found in list of available engines: "
+                f"'{engine}' not found in list of available engines: "
                 f"{list(engine_catalogue.keys())}")
             raise ValueError(errormsg)
 
         # Check if the engine has data cached already, otherwise load it
-        if engine_catalogue[enginename]["dataframes"] == {}:
-            engine_catalogue[enginename]["dataframes"] = pd.read_excel(
-                engine_catalogue[enginename]["path"], sheet_name=None)
+        if engine_catalogue[engine]["dataframes"] == {}:
+            engine_catalogue[engine]["dataframes"] = pd.read_excel(
+                engine_catalogue[engine]["path"], sheet_name=None)
 
-        self.name = enginename
-        self.type = engine_catalogue[enginename]["type"]
-        self._dataframes = engine_catalogue[enginename]["dataframes"]
+        self.name = engine
+        self.type = engine_catalogue[engine]["type"]
+        self._dataframes = engine_catalogue[engine]["dataframes"]
 
         build_deck(self)
         return
@@ -83,7 +123,7 @@ class EngineDeck:
         """The raw dataframes extracted from engine data files."""
         return self._dataframes
 
-    def thrust(self, mach, altitude, norm: bool = None):
+    def thrust(self, mach, altitude, *, norm: bool = None):
         """
         Return the thrust available at the given flight conditions.
 
@@ -94,7 +134,9 @@ class EngineDeck:
                 thrust. Optional, defaults to False (no normalisation).
 
         Returns:
-            The thrust developed by the propulsion system, in Newtons.
+            The thrust developed by the propulsion system, in Newtons. If
+            norm = True, this is just the ratio of available thrust to a static
+            sea-level performance datum.
 
         """
         # Recast as necessary
@@ -109,7 +151,7 @@ class EngineDeck:
 
         return thrust
 
-    def thrust_slto(self, mach, norm: bool = None):
+    def thrust_slto(self, mach, *, norm: bool = None):
         """
         Return the (augmented) thrust during takeoff, at sea level.
 
@@ -119,7 +161,9 @@ class EngineDeck:
                 thrust. Optional, defaults to False (no normalisation).
 
         Returns:
-            The thrust developed by the propulsion system, in Newtons.
+            The thrust developed by the propulsion system, in Newtons. If
+            norm = True, this is just the ratio of available thrust to a static
+            sea-level performance datum.
 
         """
         # Recast as necessary
@@ -137,7 +181,396 @@ class EngineDeck:
         return thrust
 
 
+def TP0ratio(mach: np.ndarray, altitude: np.ndarray, atmosphere=None):
+    """
+    Compute the stagnation temperature and pressure ratio (to sea-level static).
+
+    Args:
+        mach: Freestream Mach number.
+        altitude: Geopotential altitude, in metres.
+        atmosphere: An atmosphere object. Optional, defaults to ISA.
+
+    Returns:
+        A tuple (theta0, delta0).
+
+    """
+    # Recast as necessary
+    atmosphere = isaref if atmosphere is None else atmosphere
+
+    # Static quantity ratios
+    temp_K = uc.c2k(isaref.airtemp_c(altitude))
+    theta = temp_K / 288.15  # static temperature ratio
+    press_Pa = isaref.airpress_pa(altitude)
+    delta = press_Pa / 101325  # static pressure ratio
+
+    # Convert to stagnation ratios
+    gamma = 1.4
+    theta0 = theta * (1 + (gamma - 1) / 2 * mach ** 2)
+    delta0 = delta * (1 + (gamma - 1) / 2 * mach ** 2) ** (gamma / (gamma - 1))
+
+    return theta0, delta0
+
+
+class TurbofanHiBPR:
+    """
+    Performance deck for a high bypass ratio, subsonic turbofan.
+
+    References:
+        -   J. D. Mattingly, W. H. Heiser, D. T. Pratt, *Aircraft Engine Design*
+            2nd ed. Reston, Virginia: AIAA, 2002. Sections 2.3.2, 3.3.2.
+    """
+    name = "generic:TurbofanHiBPR"
+    type = "turbofan"
+
+    @classmethod
+    def thrust(cls, mach, altitude, *, norm: bool = True, **kwargs):
+        """
+        Return the thrust available at the given flight conditions.
+
+        Args:
+            mach: Flight Mach number.
+            altitude: Flight level (above mean sea level), in metres.
+            norm: For generic propulsion system types, this must be set to True.
+
+        Keyword Args:
+            atmosphere: Alternative atmosphere object, if one is desired when
+                computing stagnation temperature and pressure ratios.
+
+        Returns:
+            The thrust developed by the propulsion system, in Newtons. If
+            norm = True, this is just the ratio of available thrust to a static
+            sea-level performance datum.
+
+        """
+        # Recast as necessary
+        mach = recastasnpfloatarray(mach)
+        altitude = recastasnpfloatarray(altitude)
+
+        if norm is not True:
+            errormsg = f"{cls.name} must use norm=True, found that {norm=}"
+            raise RuntimeError(errormsg)
+
+        theta0, delta0 = TP0ratio(mach, altitude, kwargs.get("atmosphere"))
+        # Assume theta break (throttle ratio) of 1.05
+        TR = 1.05
+
+        lapse = delta0 * (1 - 0.49 * mach ** 0.5)
+        slice = theta0 > TR
+        lapse[slice] -= (delta0 * 3 * (theta0 - TR) / (1.5 + mach))[slice]
+
+        return np.clip(lapse, 0, None)
+
+    @classmethod
+    def thrust_slto(cls, mach, **kwargs):
+        """
+        Return the takeoff thrust at sea level.
+
+        Args:
+            mach: Freestream Mach number.
+
+        Keyword Args:
+            norm: For generic propulsion system types, this must be set to True.
+            atmosphere: Alternative atmosphere object, if one is desired when
+                computing stagnation temperature and pressure ratios.
+
+        Returns:
+            The thrust developed by the propulsion system, in Newtons. If
+            norm = True, this is just the ratio of available thrust to a static
+            sea-level performance datum.
+
+        Notes:
+            Since this is an interpretation of a modern high bypass ratio
+            turbofan engine, there is no thrust augmentation to speak of.
+
+        """
+        return cls.thrust(mach, altitude=0.0, **kwargs)
+
+
+class TurbofanLoBPR:
+    """
+    Performance deck for a low bypass ratio, mixed flow turbofan.
+
+    References:
+    -   J. D. Mattingly, W. H. Heiser, D. T. Pratt, *Aircraft Engine Design*
+       2nd ed. Reston, Virginia: AIAA, 2002. Sections 2.3.2, 3.3.2.
+
+    """
+    name = "generic:TurbofanLoBPR"
+    type = "turbofan"
+
+    @classmethod
+    def thrust(cls, mach, altitude, *, norm: bool = True, **kwargs):
+        """
+        Return the thrust available at the given flight conditions.
+
+        Args:
+            mach: Flight Mach number.
+            altitude: Flight level (above mean sea level), in metres.
+            norm: For generic propulsion system types, this must be set to True.
+
+        Keyword Args:
+            atmosphere: Alternative atmosphere object, if one is desired when
+                computing stagnation temperature and pressure ratios.
+
+        Returns:
+            The thrust developed by the propulsion system, in Newtons. If
+            norm = True, this is just the ratio of available thrust to a static
+            sea-level performance datum.
+
+        """
+        # Recast as necessary
+        mach = recastasnpfloatarray(mach)
+        altitude = recastasnpfloatarray(altitude)
+
+        if norm is not True:
+            errormsg = f"{cls.name} must use norm=True, found that {norm=}"
+            raise RuntimeError(errormsg)
+
+        theta0, delta0 = TP0ratio(mach, altitude, kwargs.get("atmosphere"))
+        # Assume theta break (throttle ratio) of 1.05
+        TR = 1.05
+
+        lapse = 0.6 * delta0
+        slice = theta0 > TR
+        lapse[slice] -= (0.6 * delta0 * 3.8 * (theta0 - TR) / theta0)[slice]
+
+        # Reframe the equation so sea-level static dry thrust is 1.0
+        lapse *= 1 / 0.6
+
+        return np.clip(lapse, 0, None)
+
+    @classmethod
+    def thrust_slto(cls, mach, *, norm: bool = True, **kwargs):
+        """
+        Return the takeoff (wet) thrust at sea level.
+
+        Args:
+            mach: Freestream Mach number.
+            norm: For generic propulsion system types, this must be set to True.
+
+        Keyword Args:
+            atmosphere: Alternative atmosphere object, if one is desired when
+                computing stagnation temperature and pressure ratios.
+
+        Returns:
+            The thrust developed by the propulsion system, in Newtons. If
+            norm = True, this is just the ratio of available thrust to a static
+            sea-level performance datum.
+
+        """
+        # Recast as necessary
+        mach = recastasnpfloatarray(mach)
+
+        if norm is not True:
+            errormsg = f"{cls.name} must use norm=True, found that {norm=}"
+            raise RuntimeError(errormsg)
+
+        theta0, delta0 = TP0ratio(mach, np.zeros(1), kwargs.get("atmosphere"))
+        # Assume theta break (throttle ratio) of 1.05
+        TR = 1.05
+
+        lapse = delta0
+        slice = theta0 > TR
+        lapse[slice] -= (delta0 * 3.5 * (theta0 - TR) / theta0)[slice]
+
+        # Reframe the equation so sea-level static dry thrust is 1.0
+        lapse *= 1 / 0.6
+
+        return np.clip(lapse, 0, None)
+
+
+class Turbojet:
+    """
+    Performance deck for a turbojet.
+
+    References:
+        -   J. D. Mattingly, W. H. Heiser, D. T. Pratt, *Aircraft Engine Design*
+            2nd ed. Reston, Virginia: AIAA, 2002. Sections 2.3.2, 3.3.2.
+
+    """
+    name = "generic:Turbojet"
+    type = "turbojet"
+
+    @classmethod
+    def thrust(cls, mach, altitude, *, norm: bool = True, **kwargs):
+        """
+        Return the thrust available at the given flight conditions.
+
+        Args:
+            mach: Flight Mach number.
+            altitude: Flight level (above mean sea level), in metres.
+            norm: For generic propulsion system types, this must be set to True.
+
+        Keyword Args:
+            atmosphere: Alternative atmosphere object, if one is desired when
+                computing stagnation temperature and pressure ratios.
+
+        Returns:
+            The thrust developed by the propulsion system, in Newtons. If
+            norm = True, this is just the ratio of available thrust to a static
+            sea-level performance datum.
+
+        """
+        # Recast as necessary
+        mach = recastasnpfloatarray(mach)
+        altitude = recastasnpfloatarray(altitude)
+
+        if norm is not True:
+            errormsg = f"{cls.name} must use norm=True, found that {norm=}"
+            raise RuntimeError(errormsg)
+
+        theta0, delta0 = TP0ratio(mach, altitude, kwargs.get("atmosphere"))
+        # Assume theta break (throttle ratio) of 1.05
+        TR = 1.05
+
+        lapse = 0.8 * delta0 * (1 - 0.16 * mach ** 0.5)
+        slice = theta0 > TR
+        lapse[slice] -= (
+                0.8 * delta0 * 24 * (theta0 - TR) / (9 + mach) / theta0)[slice]
+
+        # Reframe the equation so sea-level static dry thrust is 1.0
+        lapse *= 1 / 0.8
+
+        return np.clip(lapse, 0, None)
+
+    @classmethod
+    def thrust_slto(cls, mach, *, norm: bool = True, **kwargs):
+        """
+        Return the takeoff (wet) thrust at sea level.
+
+        Args:
+            mach: Freestream Mach number.
+            norm: For generic propulsion system types, this must be set to True.
+
+        Keyword Args:
+            atmosphere: Alternative atmosphere object, if one is desired when
+                computing stagnation temperature and pressure ratios.
+
+        Returns:
+            The thrust developed by the propulsion system, in Newtons. If
+            norm = True, this is just the ratio of available thrust to a static
+            sea-level performance datum.
+
+        """
+        # Recast as necessary
+        mach = recastasnpfloatarray(mach)
+
+        if norm is not True:
+            errormsg = f"{cls.name} must use norm=True, found that {norm=}"
+            raise RuntimeError(errormsg)
+
+        theta0, delta0 = TP0ratio(mach, np.zeros(1), kwargs.get("atmosphere"))
+        # Assume theta break (throttle ratio) of 1.05
+        TR = 1.05
+
+        lapse = delta0 * (1 - 0.3 * (theta0 - 1) - 0.1 * mach ** 0.5)
+        slice = theta0 > TR
+        lapse[slice] -= (delta0 * 1.5 * (theta0 - TR) / theta0)[slice]
+
+        # Reframe the equation so sea-level static dry thrust is 1.0
+        lapse *= 1 / 0.8
+
+        return np.clip(lapse, 0, None)
+
+
+class Turboprop:
+    """
+    Performance deck for a turboprop engine.
+
+    References:
+        -   J. D. Mattingly, W. H. Heiser, D. T. Pratt, *Aircraft Engine Design*
+            2nd ed. Reston, Virginia: AIAA, 2002. Sections 2.3.2, 3.3.2.
+
+    """
+    name = "generic:Turboprop"
+    type = "turboprop"
+
+    @classmethod
+    def thrust(cls, mach, altitude, *, norm: bool = True, **kwargs):
+        """
+        Return the thrust available at the given flight conditions.
+
+        Args:
+            mach: Flight Mach number.
+            altitude: Flight level (above mean sea level), in metres.
+            norm: For generic propulsion system types, this must be set to True.
+
+        Keyword Args:
+            atmosphere: Alternative atmosphere object, if one is desired when
+                computing stagnation temperature and pressure ratios.
+
+        Returns:
+            The thrust developed by the propulsion system, in Newtons. If
+            norm = True, this is just the ratio of available thrust to a static
+            sea-level performance datum.
+
+        """
+        # Recast as necessary
+        mach = recastasnpfloatarray(mach)
+        altitude = recastasnpfloatarray(altitude)
+
+        if norm is not True:
+            errormsg = f"{cls.name} must use norm=True, found that {norm=}"
+            raise RuntimeError(errormsg)
+
+        theta0, delta0 = TP0ratio(mach, altitude, kwargs.get("atmosphere"))
+        # Assume theta break (throttle ratio) of 1.05
+        TR = 1.05
+
+        # I think Mattingly makes a mistake below in writing M-1 instead of M-.1
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            lapse = delta0 * (1 - 0.96 * (mach - 0.1) ** 0.25)
+            slice = theta0 > TR
+            lapse[slice] -= \
+                (delta0 * 3 * (theta0 - TR) / 8.13 / (mach - 0.1))[slice]
+            lapse[mach <= 0.1] = delta0[mach <= 0.1]
+
+        return np.clip(lapse, 0, None)
+
+    @classmethod
+    def thrust_slto(cls, mach, **kwargs):
+        """
+        Return the takeoff thrust at sea level.
+
+        Args:
+            mach: Freestream Mach number.
+
+        Keyword Args:
+            norm: For generic propulsion system types, this must be set to True.
+            atmosphere: Alternative atmosphere object, if one is desired when
+                computing stagnation temperature and pressure ratios.
+
+        Returns:
+            The thrust developed by the propulsion system, in Newtons. If
+            norm = True, this is just the ratio of available thrust to a static
+            sea-level performance datum.
+
+        Notes:
+            Since this is an interpretation of a modern turboprop engine, there
+            is no thrust augmentation to speak of.
+
+        """
+        return cls.thrust(mach, altitude=0.0, **kwargs)
+
+
+class Piston:
+    pass
+
+
 def build_deck(deck: EngineDeck) -> None:
+    """
+    Given an EngineDeck object for a real engine with data, use said data to
+    create interpolation engines for basic performance attributes like thrust
+    and power (where necessary).
+
+    Args:
+        deck: An EngineDeck object with associated performance dataframes.
+
+    Returns:
+        None.
+
+    """
     # Start with generic methods that apply to all deck types
 
     # Generic "Deck" sheets should "unpack" themselves
