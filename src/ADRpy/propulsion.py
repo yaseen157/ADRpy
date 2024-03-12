3edcbd16c7b1439c625cd69df716e2ab38e153d1
Yaseen Reza <57412770+yaseen157@users.noreply.github.com>
2024-03-12 09:45:33 +0000

    Bug fix for Mac OS users (ignore .DS_Store and non dirs)

--- src/ADRpy/propulsion.py
+++ src/ADRpy/propulsion.py
@@ -24,7 +24,8 @@ engine_data_path = (
     os.path.join(os.path.dirname(__file__), "data", "engine_data"))
 engine_deck_types = [
     x for x in os.listdir(engine_data_path)  # List all directories on path...
-    if os.path.isdir(x) and not x.startswith(".")  # if they are public folders
+    if os.path.isdir(os.path.join(engine_data_path, x))
+       and not x.startswith(".")
 ]
 
 # Catalogue (paths to) decks
